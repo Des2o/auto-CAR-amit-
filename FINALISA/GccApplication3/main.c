cbdfe36eb7eb131c0934e0a9e2f65cab3eaac7e7
Des2o <73612990+Des2o@users.noreply.github.com>
2020-10-29 16:01:59 +0200

    Add files via upload

--- FINALISA/GccApplication3/main.c
+++ FINALISA/GccApplication3/main.c
@@ -4,7 +4,7 @@
  * Created: 28/10/2020 11:37:50 ص
  * Author : Qaiaty_Store
  */ 
-#define F_CPU 16000000UL
+#define F_CPU 8000000UL
 #include <avr/io.h>
 #include <util/delay.h>
 #include <avr/interrupt.h>
@@ -28,36 +28,47 @@ car_init();
 
   DDRC &= ~((1<<DDC5)|(1<<DDC4));
     /* Replace with your application code */
-
+//double us =0;
 	while (1) 
     {
-
-
-	if(cm_Distance()>10)
+		//sampling for real hardware
+/*for(int i =0 ;i<20;i++)
+{
+	us += cm_Distance();
+}
+us /=20 ;*/
+	if(cm_Distance()>15.0)
 	{
+		
 		char f = ((PINC & 0b00110000)>>4);
-		_delay_ms(100);
-		if(f !=((PINC & 0b00110000)>>4) )f=0;
+		/*debounce
+		_delay_ms(20);
+		if(f !=((PINC & 0b00110000)>>4) )f=0;*/
+	
+	
+	//in track w  mafe4 obstacles
 		if(f==0)carForward(150);
 		else
 		{
 			Brake();
-			_delay_ms(1000);
+			//_delay_ms(1000);
 			if(f==3)
 			{
 				carReverse(150);
-				_delay_ms(1000);
+				//_delay_ms(1000);
 			}
+			//Track border left
 			if(f==2)
 			{
 				carRightF();
-				_delay_ms(700);
+				//_delay_ms(700);
 				
 			}
+			//track border right
 			if(f==1)
 			{
 				carLeftF();
-				_delay_ms(700);
+				//_delay_ms(700);
 			}
 			
 		}
@@ -66,34 +77,42 @@ car_init();
 	else
 	{
 		char f = ((PINC & 0b00110000)>>4);
-		_delay_ms(100);
-		if(f !=((PINC & 0b00110000)>>4) )f=0;
+		//debounce
+		//_delay_ms(20);
+		//if(f !=((PINC & 0b00110000)>>4) )f=0;
 		Brake();
-		_delay_ms(1000);
+		//_delay_ms(1000);
+		//obstacle + track border right
 		if(f==1)
 		{
 			carLeftB();
-			_delay_ms(700);
+			//_delay_ms(700);
 		}
+		//obstacle + track border left
 		if(f==2)
 		{
 			carRightB();
-			_delay_ms(700);
+			//_delay_ms(700);
 		}
+		//obstacle in track
 		else
 		{
 			carReverse(150);
-		    _delay_ms(500);
+		    //_delay_ms(500);
+			
+			
 			carRightB();
-			_delay_ms(700);
+			//_delay_ms(700);
 			
 		}
 	}
+	
+	//testing
 /*Brake();
 _delay_ms(1000);
- carForward(180);
+ carForward(250);
 _delay_ms(1000);
-carReverse(180);
+carReverse(250);
 _delay_ms(1000);
 carLeftF();
 _delay_ms(700);
@@ -103,13 +122,11 @@ carLeftB();
 _delay_ms(700);
 carRightB();
 _delay_ms(700);*/
-	
 
-	
-		}
+
 		
 			
-		
+	}
     
 }
 
